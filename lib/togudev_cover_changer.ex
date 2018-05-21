2efa26da67f893fb4911fb297bee5e62cd4b8a2a
george <george>
2018-05-21 19:37:12 +1000

    init commit

---
+++ lib/togudev_cover_changer.ex
@@ -0,0 +1,18 @@
+defmodule TogudevCoverChanger do
+  @moduledoc """
+  Documentation for TogudevCoverChanger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TogudevCoverChanger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/togudev_cover_changer_test.exs
@@ -0,0 +1,8 @@
+defmodule TogudevCoverChangerTest do
+  use ExUnit.Case
+  doctest TogudevCoverChanger
+
+  test "greets the world" do
+    assert TogudevCoverChanger.hello() == :world
+  end
+end
