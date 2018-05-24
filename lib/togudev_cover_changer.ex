9e9a6705e49cd08f15f5eaf26965e8daaaa707ac
george <george>
2018-05-24 18:27:07 +1000

    reworking architecture

--- lib/togudev_cover_changer.ex
+++ lib/togudev_cover_changer.ex
@@ -3,10 +3,7 @@ defmodule TogudevCoverChanger do
   use Supervisor
 
   def start(_type, _args) do
-    children = [
-      %{id: UploadUrlBuilder, start: {UploadUrlBuilder, :start_link, [[]]}}
-    ]
-    Supervisor.start_link(children, [strategy: :one_for_one])
+
   end
 
 end
