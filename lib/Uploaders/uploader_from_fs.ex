854b08d5a0f376e39a0aa7d2cae68fb3b7a7d8bc
George Warden <georgwarden@gmail.com>
2018-05-30 13:15:33 +1000

    yup, I've done something

--- lib/Uploaders/uploader_from_fs.ex
+++ lib/Uploaders/uploader_from_fs.ex
@@ -1,7 +1,25 @@
 defimpl Uploaders.UploaderFromFs, for: string do
-  
+    use Utils.CredsAware
+    require Logger
+
+    @base_url "https://api.vk.com/method/"
+    @get_upload_url "#{@base_url}photo.???" # TODO
+    @upload_confirm_url "#{@base_url}photo???" # TODO
+
     def upload(path_to_file) do
-        
+        {group_id, api_key} = get_group_creds
+
     end
+
+    defp get_upload_url(group_id, api_key) do
+        case HTTPoison.get("#{@get_upload_url}owner_id=#{group_id}") do
+            {:ok, %HTTPoison.Response{body: body}} ->
+                ExJSON.parse(body, :to_map)
+                |> Map.get("response")
+                |> Map.get("upload_url") # TODO
+            {:error, error} -> Logger.error "Unable to process request at UploaderFromFs to fetch upload_url"
+        end
+    end
+
 
 end
