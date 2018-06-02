f4feb34f1d229ad5dd91ae2984fa76beb6fbe068
George Warden <georgwarden@gmail.com>
2018-06-02 12:29:32 +1000

    add service key to list of credentials

--- lib/Utils/creds_aware.ex
+++ lib/Utils/creds_aware.ex
@@ -2,12 +2,18 @@ defmodule Utils.CredsAware do
     
     defmacro __using__(_) do
         quote do
+
             def get_group_creds() do
                 {
                     Application.get_env(:togudev_cover_changer, :group_id),
                     Application.get_env(:togudev_cover_changer, :api_key)
                 }
             end
+
+            def get_service_key() do
+                Application.get_env(:togudev_cover_changer, :service_key)
+            end
+        
         end
     end
 
