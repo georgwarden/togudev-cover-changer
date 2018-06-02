f80059755e1d920facf21d9dc3aaa48e3abf6e92
george <george>
2018-06-02 22:43:22 +1000

    make an interface for KallingDrawer to call java

--- lib/Drawers/kalling_drawer.ex
+++ lib/Drawers/kalling_drawer.ex
@@ -1,7 +1,8 @@
 defmodule Drawers.KallingDrawer do
 
     def real_draw(data) do
-        
+        args = Enum.map data, fn user_and_likes -> Integer.to_string(elem(user_and_likes, 0)) <> Integer.to_string(elem(user_and_likes, 1)) end
+        System.cmd "java", ["-jar" | args]
     end
 
 end
