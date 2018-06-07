4750e3b241196bb82001b1efbb1d8224910e63a0
George Warden <georgwarden@gmail.com>
2018-06-07 11:56:51 +1000

    clarify the call target

--- lib/Drawers/kalling_drawer.ex
+++ lib/Drawers/kalling_drawer.ex
@@ -3,7 +3,7 @@ defmodule Drawers.KallingDrawer do
     def real_draw(data) do
         serialized = ExJSON.generate data
         trimmed = serialized |> String.replace(" ", "") |> String.replace("\n", "")
-        System.cmd "java", ["-jar" | trimmed]
+        System.cmd "java", ["-jar kallingdrawerimpl.jar" | trimmed]
     end
 
 end
